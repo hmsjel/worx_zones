8ad22f49dd38f44674c38595d91cfb8296ecd7bc
Hmsjel <90280474+hmsjel@users.noreply.github.com>
2026-06-24 11:14:26 +0200

    Add configuration flow for Worx Zones integration

---
+++ custom_components/worx_zones/config_flow.py
@@ -0,0 +1,22 @@
+import voluptuous as vol
+from homeassistant import config_entries
+
+DOMAIN = "worx_zones"
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Worx Zones",
+                data=user_input
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("username"): str,
+                vol.Required("password"): str,
+            })
+        )
